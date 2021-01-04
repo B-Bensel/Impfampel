6b92a13fe6d9f612a4bf8a1fc4566d679a23b09b
B-Bensel <B-Bensel@users.noreply.github.com>
2021-01-04 22:28:15 +0100

    Update index.php

--- Server/index.php
+++ Server/index.php
@@ -1,5 +1,50 @@
-<html>
-<body>
-<a href="dashboard.php">Dashboard</a>
-</body>
+<!doctype html>
+<html class="h-100" lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Bootstrap CSS -->
+		<link href="style/bootstrap.min.css" rel="stylesheet">
+
+		<title>Impfampel</title>
+	</head>
+	<body class="d-flex flex-column h-100">
+    <div class="container-fluid mb-2">
+	    <div class="row text-center">
+		    <div class="col-3">
+			    Logo
+		    </div>
+		    <div id="title" class="col-6">
+			    <h1>Impfampel</h1>
+		    </div>
+		    <div class="col-3">
+  			  Logo
+	  	  </div>
+  	  </div>
+    </div>
+		<div class="container-fluid">
+			<div class="row text-center">
+				<div class="col mx-auto">
+					<p>
+						<a href="dashboard.php">Dashboard Ansicht</a>
+					</p>
+					<p>
+						<a href="mobile.php">Mobile Ansicht</a>
+					</p>
+				</div>
+			</div>
+		</div>
+		<footer class="footer mt-auto py-3 bg-light">
+			<div class="container text-center">
+				<span class="text-muted">
+					Impfampel: <a href="https://www.github.com/B-Bensel/Impfampel">www.github.com/B-Bensel/Impfampel</a>
+				</span>
+			</div>
+		</footer>
+
+		<script src="js/popper.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
 </html>
